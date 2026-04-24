2f7c41d73da1ac7178dcfbb7894cd9f79e4746f2
L0Rd-Kevinho <ka.vieira10@gmail.com>
2026-04-24 16:09:56 -0500

    Update Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -128,7 +128,7 @@
 		</ul>
 	</div>
 	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT -->
-	<span class="copyright">&copy; {year} <a href="https://github.com/nmelhado/league-page">League Page</a></span>
+	<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/nmelhado/league-page">League Page</a></span>
 	<br />
 	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
 	<span class="creator">Built by Kevin Vieira<br /></span>
